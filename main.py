aec0343aa9fada8f48e6a6ffb5a634b0da2767c1
VulpesVolcan <noahriddle743@gmail.com>
2026-05-11 19:28:29 -0700

    Added player.py to handle drawing player object

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"- 'Screen width: {SCREEN_WIDTH}', - 'Screen height: {SCREEN_HEIGHT}'")
@@ -8,13 +9,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0
     timer = pygame.time.Clock()
+    ship = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
-        
+     
+
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        ship.draw(screen)
         pygame.display.flip()
         dt = timer.tick(60) / 1000
 
