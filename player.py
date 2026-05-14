760aa42fbc53f1d1dcb18d4400c35229ce5b15aa
VulpesVolcan <noahriddle743@gmail.com>
2026-05-13 19:49:18 -0700

    Added cooldown to shoot method

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS,LINE_WIDTH,PLAYER_TURN_SPEED,PLAYER_SPEED,PLAYER_SHOOT_SPEED,SHOT_RADIUS
+from constants import PLAYER_RADIUS,LINE_WIDTH,PLAYER_TURN_SPEED,PLAYER_SPEED,PLAYER_SHOOT_SPEED,SHOT_RADIUS,PLAYER_SHOOT_COOLDOWN_SECONDS
 from shot import Shot
 color = "white"
 
@@ -8,6 +8,8 @@ class Player(CircleShape):
     def __init__(self,x,y):
         super().__init__(x,y,PLAYER_RADIUS)
         self.rotation = 0
+        self.cooldown = 0
+       
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -24,6 +26,7 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt 
 
     def update(self, dt):
+        self.cooldown -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -49,6 +52,9 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
     
     def shoot(self):
+        if self.cooldown > 0:
+            return
+        self.cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
         bullet = Shot(self.position[0],self.position[1],SHOT_RADIUS)
         bullet_vector = pygame.Vector2(0, 1)
         rotated_bullet_vector = bullet_vector.rotate(self.rotation)
